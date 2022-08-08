74e5758527c4765fb072a5c71c53c28067ab726f
=recedivies <ahmadhiprananta@gmail.com>
2022-08-08 17:35:12 +0700

    style: added style index

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,42 +1,14 @@
-import React, { useState } from "react";
-
-import AxiosClient from "../api/axiosClient";
+import NotificationProvider from "../context/notifContext";
+import ThemeProvider from "../context/themeContext";
+import Router from "../routing/Router";
 
 function App() {
-  const [textInput, setTextInput] = useState("");
-  const [output, setOutput] = useState("");
-
-  const handleSubmit = () => {
-    AxiosClient.get(`/api/test?text=${textInput}`)
-      .then((res) => {
-        setOutput(res.data.text);
-      })
-      .catch((err) => console.log(err));
-  };
-
   return (
-    <div>
-      <h1 className="font-bold underline">Hello world!</h1>
-      <header>
-        <pre>django-react-heroku-template</pre>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-
-        <div>
-          <p>Test connection with API:</p>
-          <label htmlFor="char-input">Make this text uppercase: </label>
-          <input
-            id="char-input"
-            type="text"
-            value={textInput}
-            onChange={(e) => setTextInput(e.target.value)}
-          />
-          <button onClick={handleSubmit}>Submit</button>
-          <h3>{output}</h3>
-        </div>
-      </header>
-    </div>
+    <ThemeProvider>
+      <NotificationProvider>
+        <Router />
+      </NotificationProvider>
+    </ThemeProvider>
   );
 }
 
