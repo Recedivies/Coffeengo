20331e0274d320187712b10463a67ab80d89833d
=recedivies <ahmadhiprananta@gmail.com>
2022-08-08 17:32:11 +0700

    feat: added pages components

---
+++ frontend/src/components/pages/CreateRoom.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Accent from "../utils/Accent";
+
+const CreateRoom: React.FC = () => {
+  return (
+    <div className="flex flex-grow flex-col items-center justify-center">
+      <Accent className="text-3xl font-bold">CreateRoom</Accent>
+    </div>
+  );
+};
+export default CreateRoom;
